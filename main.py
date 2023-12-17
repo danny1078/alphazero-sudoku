3726a7e8f3c4498c7521526eee38bf7fb458e286
Danny Han <handanny2014@gmail.com>
2023-12-17 01:24:35 -0500

    THANK YOU QIBIN

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import torch.multiprocessing
 import numpy as np
 
 args = {
-    'maxNumIters': 10,
+    'maxNumIters': 20,
     'numMCTSSims': 200,  # Number of games moves for MCTS to simulate.
     'cpuct': 1,  # UCB hyperparameter
     'numGames': 500,  # number of games to play in one iteration
@@ -22,7 +22,7 @@ args = {
     'temp': 0.8,
     'device': torch.device('cuda' if torch.cuda.is_available() else 'cpu'),
     'conf_offset': 0.5,
-    'start_blanks': 9,
+    'start_blanks': 2,
     'end_blanks': 12,
 }
 
@@ -44,7 +44,7 @@ def main(filename=None):
         logger.log({"num_blanks": num_blanks})
         train_net(net, data, args, logger)
         iters_so_far += 1
-        if (percent_perfect_games > 0.97 and iters_so_far >= 5) or (iters_so_far >= args['maxNumIters']):
+        if (percent_perfect_games > 0.98 and iters_so_far >= 10) or (iters_so_far >= args['maxNumIters']):
             torch.save(net.state_dict(), 'checkpoints/post_' + str(num_blanks) + '.pth')
             num_blanks += 1
             iters_so_far = 0
@@ -55,4 +55,4 @@ def main(filename=None):
 
 
 if __name__ == "__main__":
-    main(filename=r'C:\Users\Danny Han\Desktop\alphazero-sudoku\checkpoints\run_8_blanks_2\checkpoint_20.pth')
+    main()
